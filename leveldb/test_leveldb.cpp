b485a32f4bfd31f7af5d73afcd3f09087b5b16a4
zhuichao001 <zhuichao001@163.com>
2020-12-30 07:42:20 +0800

    practice leveldb

--- leveldb/test_leveldb.cpp
+++ leveldb/test_leveldb.cpp
@@ -5,7 +5,6 @@
 #include <leveldb/db.h>
 #include <leveldb/status.h>
 
-using namespace leveldb;
 
 int main(void) {
     leveldb::DB *db = nullptr;
@@ -13,17 +12,17 @@ int main(void) {
 
     options.create_if_missing = true;
 
-    leveldb::Status status = DB::Open(options, "/tmp/testdb", &db);
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
     assert(status.ok());
 
     std::string key = "foo";
     std::string val = "bar";
     std::string out;
 
-    leveldb::Status s = db->Put(WriteOptions(), key, val);
+    leveldb::Status s = db->Put(leveldb::WriteOptions(), key, val);
 
     if (s.ok()){
-        s = db->Get(ReadOptions(), key, &out);
+        s = db->Get(leveldb::ReadOptions(), key, &out);
     }
 
     if (s.ok()){
